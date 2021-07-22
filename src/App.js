136641e2db257128028fbc673d3c711705001252
DeMonki <dave@fretlow.com>
2021-07-22 18:42:24 -0500

    componentized homepage and styled menu-item

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import HomePage from './homepage.component';
+import Directory from './components/directory/directory.component';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Directory />
     </div>
   );
 }
