da8236bbd53072cbb210f569af32849435ee9d4a
DeMonki <dave@fretlow.com>
2021-07-23 12:20:05 -0500

    shop page, header added, with styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import {  Route, Switch } from 'react-router-dom';
 import  HomePage  from './pages/homepage.component'
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 import './App.css';
 
 
 
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-)
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
       <Route exact path='/' component={HomePage}/>
-      <Route exacts path='/shop/hats' component={HatsPage}/>
+      <Route exacts path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
